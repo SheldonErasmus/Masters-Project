c7578b07ac968ccc2e43ff5ea0676d7d4cf28836
SheldonErasmus <sheldon.erasmus@gmail.com>
2022-04-29 12:55:15 +0000

    upadte 29 April

--- catkin_ws/src/simu_hexapod_stuff/src/WorldFeetPositions_V2.py
+++ catkin_ws/src/simu_hexapod_stuff/src/WorldFeetPositions_V2.py
@@ -9,35 +9,35 @@ from geometry_msgs.msg import Pose, Point
 from my_message.msg import LegPath, WorldFeetPlace
 from std_msgs.msg import Float32
 
-XPath = [[None for i in range(2)] for i in range(6)]
-YPath = [[None for i in range(2)] for i in range(6)]
-ZPath = [[None for i in range(2)] for i in range(6)]
+XPath = [[None for i in range(3)] for i in range(6)]
+YPath = [[None for i in range(3)] for i in range(6)]
+ZPath = [[None for i in range(3)] for i in range(6)]
 StepS_x = 0; StepS_y = 0; TurnPath = 0
 FeetChangedFlag = 0
 def path_cb(msg):
     global StepS_x, StepS_y, TurnPath, FeetChangedFlag
     FeetChangedFlag = 1
-    for col in [0,6]:
-        XPath[0][col%5]=msg.PathL0x[col]
-        XPath[1][col%5]=msg.PathL1x[col]
-        XPath[2][col%5]=msg.PathL2x[col]
-        XPath[3][col%5]=msg.PathL3x[col]
-        XPath[4][col%5]=msg.PathL4x[col]
-        XPath[5][col%5]=msg.PathL5x[col]
+    for col in [0,3,6]:
+        XPath[0][col//3]=msg.PathL0x[col]
+        XPath[1][col//3]=msg.PathL1x[col]
+        XPath[2][col//3]=msg.PathL2x[col]
+        XPath[3][col//3]=msg.PathL3x[col]
+        XPath[4][col//3]=msg.PathL4x[col]
+        XPath[5][col//3]=msg.PathL5x[col]
 
-        YPath[0][col%5]=msg.PathL0y[col]
-        YPath[1][col%5]=msg.PathL1y[col]
-        YPath[2][col%5]=msg.PathL2y[col]
-        YPath[3][col%5]=msg.PathL3y[col]
-        YPath[4][col%5]=msg.PathL4y[col]
-        YPath[5][col%5]=msg.PathL5y[col]
+        YPath[0][col//3]=msg.PathL0y[col]
+        YPath[1][col//3]=msg.PathL1y[col]
+        YPath[2][col//3]=msg.PathL2y[col]
+        YPath[3][col//3]=msg.PathL3y[col]
+        YPath[4][col//3]=msg.PathL4y[col]
+        YPath[5][col//3]=msg.PathL5y[col]
 
-        ZPath[0][col%5]=msg.PathL0z[col]
-        ZPath[1][col%5]=msg.PathL1z[col]
-        ZPath[2][col%5]=msg.PathL2z[col]
-        ZPath[3][col%5]=msg.PathL3z[col]
-        ZPath[4][col%5]=msg.PathL4z[col]
-        ZPath[5][col%5]=msg.PathL5z[col]
+        ZPath[0][col//3]=msg.PathL0z[col]
+        ZPath[1][col//3]=msg.PathL1z[col]
+        ZPath[2][col//3]=msg.PathL2z[col]
+        ZPath[3][col//3]=msg.PathL3z[col]
+        ZPath[4][col//3]=msg.PathL4z[col]
+        ZPath[5][col//3]=msg.PathL5z[col]
 
         TurnPath = msg.PathAng[6]
 
@@ -45,17 +45,17 @@ def path_cb(msg):
         
     yaw_rad = TurnPath * pi/180.0
     for i in range(6):
-        existingAngle = arctan2(YPath[i][1],XPath[i][1])
+        existingAngle = arctan2(YPath[i][2],XPath[i][2])
 
-        radius = sqrt(XPath[i][1]**2+YPath[i][1]**2)
+        radius = sqrt(XPath[i][2]**2+YPath[i][2]**2)
 
         demandYaw = existingAngle + yaw_rad
 
-        XPath[i][1] = radius*cos(demandYaw)
-        YPath[i][1] = radius*sin(demandYaw)
+        XPath[i][2] = radius*cos(demandYaw)
+        YPath[i][2] = radius*sin(demandYaw)
 
-    StepS_x = round(abs(XPath[0][1]-XPath[0][0])/1000,3)
-    StepS_y = round((YPath[0][1]-YPath[0][0])/1000,3)
+    StepS_x = round(abs(XPath[0][2]-XPath[0][0])/1000,3)
+    StepS_y = round((YPath[0][2]-YPath[0][0])/1000,3)
 
 n = 0.0
 e = 0.0
@@ -72,8 +72,8 @@ feetOnFloor = [1,5]
 def FeetOnFloor_cb(msg):
     global FeetOnFloorFlag,feetOnFloor
     FeetOnFloorFlag = 1
-    if feetOnFloor[0] == 4: feetOnFloor = [1,5]
-    elif feetOnFloor[0] == 1: feetOnFloor = [4,2]
+    if feetOnFloor[0] == 2: feetOnFloor = [1,5]
+    elif feetOnFloor[0] == 1: feetOnFloor = [2,4]
 
 roll=0;pitch=0;yaw=0
 def imu_cb(msg):
@@ -101,28 +101,42 @@ if __name__ == '__main__':
     spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
     delete_model_client = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
 
+    num = -1
     for k in range(6):
+        #num = num +1
         FeetPlace.XPlace[k]  = (XPath[k][1]/1000)*cos(yaw) - (YPath[k][1]/1000)*sin(yaw) + n
         FeetPlace.YPlace[k]  = (XPath[k][1]/1000)*sin(yaw) + (YPath[k][1]/1000)*cos(yaw) + (-e)
+        #spawn_model_client(model_name='F'+str(k)+'P'+str(num),model_xml=open('/home/devlon/.gazebo/models/washer/model.sdf', 'r').read(),robot_namespace='F'+str(k)+'P'+str(num),initial_pose=Pose(position=Point(FeetPlace.XPlace[k],FeetPlace.YPlace[k],0)),reference_frame='world')
 
+    
     while not rospy.is_shutdown():
 
         if FeetOnFloorFlag == 1:
             FeetOnFloorFlag = 0
 
-            phi_L = arctan2((XPath[feetOnFloor[1]][1]-XPath[feetOnFloor[0]][1]),(YPath[feetOnFloor[1]][1]-YPath[feetOnFloor[0]][1]))
+            phi_LB = arctan2((XPath[feetOnFloor[1]][0]-XPath[feetOnFloor[0]][0]),(YPath[feetOnFloor[1]][0]-YPath[feetOnFloor[0]][0]))
+            phi_LW = -arctan2((FeetPlace.XPlace[feetOnFloor[1]]-FeetPlace.XPlace[feetOnFloor[0]]),(FeetPlace.YPlace[feetOnFloor[1]]-FeetPlace.YPlace[feetOnFloor[0]]))
 
             if feetOnFloor[0] == 1: LoopRange = [0,2,4]
-            elif feetOnFloor[0] == 4: LoopRange = [1,3,5]
+            elif feetOnFloor[0] == 2: LoopRange = [1,3,5]
+
+            num = num +1
 
             for k in LoopRange:
-                deltaX_B = XPath[k][1]-XPath[feetOnFloor[0]][1]
-                deltaY_B = YPath[k][1]-YPath[feetOnFloor[0]][1]
+                deltaX_B = XPath[k][2]-XPath[feetOnFloor[0]][0]
+                deltaY_B = YPath[k][2]-YPath[feetOnFloor[0]][0]
 
-                deltaX_L = cos(phi_L)*deltaX_B + sin(phi_L)*deltaY_B
-                deltaY_L = -sin(phi_L)*deltaX_B + cos(phi_L)*deltaY_B
+                deltaX_L = cos(phi_LB)*deltaX_B + sin(phi_LB)*deltaY_B
+                deltaY_L = -sin(phi_LB)*deltaX_B + cos(phi_LB)*deltaY_B
 
-                #FootX_L = 
+                deltaX_W = cos(phi_LW)*deltaX_L - sin(phi_LW)*deltaY_L
+                deltaY_W = sin(phi_LW)*deltaX_L + cos(phi_LW)*deltaY_L
+
+                FeetPlace.XPlace[k] = deltaX_W/1000 + FeetPlace.XPlace[feetOnFloor[0]]
+                FeetPlace.YPlace[k] = deltaY_W/1000 + FeetPlace.YPlace[feetOnFloor[0]]
+
+                spawn_model_client(model_name='F'+str(k)+'P'+str(num),model_xml=open('/home/devlon/.gazebo/models/washer/model.sdf', 'r').read(),robot_namespace='F'+str(k)+'P'+str(num),initial_pose=Pose(position=Point(FeetPlace.XPlace[k],FeetPlace.YPlace[k],0)),reference_frame='world')
+                
 
 
         
