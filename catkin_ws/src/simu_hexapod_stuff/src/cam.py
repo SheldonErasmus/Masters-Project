ee6130aafc880a0e7ec4a5a84044a3246d8aa9b8
SheldonErasmus <sheldon.erasmus@gmail.com>
2022-05-10 15:08:10 +0000

    update 10 Mei

--- catkin_ws/src/simu_hexapod_stuff/src/cam.py
+++ catkin_ws/src/simu_hexapod_stuff/src/cam.py
@@ -1,96 +1,64 @@
 #!/usr/bin/env python3
-#Using the acquired binocular image for positioning
 
-import rospy
-import cv2
-import message_filters
-from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import Image
-import math #Calculate Tangent
-from matplotlib import pyplot as plt
 import numpy as np
+import rospy
+import ros_numpy
+from sensor_msgs.msg import Image as msg_Image
+from sensor_msgs.msg import CameraInfo, NavSatFix, Imu, PointCloud2
+from gazebo_msgs.srv import GetModelState
+from wgs2ned import WGS2NED
+from ToEulerAngles import ToEulerAng
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+import pyrealsense2 as rs2
+from hexapodC import HexapodC
+from my_message.msg import PathVar_n_cmdVel, WorldFeetPlace
+from std_msgs.msg import Float32
 
-bridge = CvBridge()
-hfov = 1.3962634
-image_width = 800
-flength = (image_width/2) / ( math.tan(hfov/2) )
-base_line = 0.2
-cx = 400
-cy = 400
+class ImageListener:
+    def __init__(self, topic):
+        self.topic = topic
+        self.bridge = CvBridge()
+        self.sub = rospy.Subscriber(self.topic, PointCloud2, self.PCCallback)
+        self.sub1 = rospy.Subscriber('/camera/depth/image_raw', msg_Image, self.imageDepthCallback)
 
-def callback(left_ros_image,right_ros_image):
-    global flength
-    global cx 
-    global cy
+    def PCCallback(self, data):
+        pc = ros_numpy.numpify(data)
+        self.pointsX = np.round(pc['x']*1000)
+        self.pointsY = np.round(pc['y']*1000)
+        self.pointsZ = np.round(pc['z']*1000)
 
-    left_cv_image = bridge.imgmsg_to_cv2(left_ros_image,"mono8")
-    right_cv_image = bridge.imgmsg_to_cv2(right_ros_image, "mono8")
-   
-   #Image display
-    cv2.imshow('left_image',left_cv_image)
-    cv2.imshow('right_image',right_cv_image)
+        self.PointsX_H = np.cos((-90-CA)*np.pi/180)*self.pointsY - np.sin((-90-CA)*np.pi/180)*self.pointsZ + HL*np.cos(AngHL)
+        self.PointsY_H = -self.pointsX + LI
+        self.PointsZ_H = np.sin((-90-CA)*np.pi/180)*self.pointsY + np.cos((-90-CA)*np.pi/180)*self.pointsZ + HL*np.sin(AngHL)
+        weee = 0
 
-    stereo = cv2.StereoBM_create()
+    def imageDepthCallback(self,data):
+        cv_img = self.bridge.imgmsg_to_cv2(data, data.encoding)
+        self.img = ros_numpy.numpify(data)
+        weee = 0
 
-    numDisparities = cv2.getTrackbarPos('numDisparities','disp')*16
-    blockSize = cv2.getTrackbarPos('blockSize','disp')*2 + 5
-    preFilterType = cv2.getTrackbarPos('preFilterType','disp')
-    preFilterSize = cv2.getTrackbarPos('preFilterSize','disp')*2 + 5
-    preFilterCap = cv2.getTrackbarPos('preFilterCap','disp')
-    textureThreshold = cv2.getTrackbarPos('textureThreshold','disp')
-    uniquenessRatio = cv2.getTrackbarPos('uniquenessRatio','disp')
-    speckleRange = cv2.getTrackbarPos('speckleRange','disp')
-    speckleWindowSize = cv2.getTrackbarPos('speckleWindowSize','disp')*2
-    disp12MaxDiff = cv2.getTrackbarPos('disp12MaxDiff','disp')
-    minDisparity = cv2.getTrackbarPos('minDisparity','disp')
+H = 300 #height of cam
+L = 0 #X offset of cam
+CA = 40 #cam angle
+LI = 17.5 #Left imager offset
 
-    stereo.setNumDisparities(numDisparities)
-    stereo.setBlockSize(blockSize)
-    stereo.setPreFilterType(preFilterType)
-    stereo.setPreFilterSize(preFilterSize)
-    stereo.setPreFilterCap(preFilterCap)
-    stereo.setTextureThreshold(textureThreshold)
-    stereo.setUniquenessRatio(uniquenessRatio)
-    stereo.setSpeckleRange(speckleRange)
-    stereo.setSpeckleWindowSize(speckleWindowSize)
-    stereo.setDisp12MaxDiff(disp12MaxDiff)
-    stereo.setMinDisparity(minDisparity)
+HL = np.sqrt(H**2+L**2) #slope of offsets
+AngHL = np.arctan2(H,L) #Angle of offsets
 
-
-    disparity = stereo.compute(left_cv_image,right_cv_image)
-    disparity = cv2.normalize(disparity, None, alpha = 0, beta = 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    # disparity = disparity.astype(np.float32)
-
-    # # Scaling down the disparity values and normalizing them
-    # disparity = (disparity/16.0 - minDisparity)/numDisparities
-
-    cv2.imshow('disp', disparity)
-    cv2.waitKey(3)
-
-def nothing(x):
-    pass
+FootX_H = 500
+FootY_H = 0
 
 if __name__ == '__main__':
-    rospy.init_node('gazebo_image_sub', anonymous=True)
+    rospy.init_node("Camera_terrain_adaptation")
+    model_coordinates = rospy.ServiceProxy( '/gazebo/get_model_state', GetModelState)
+    topic = '/camera/depth/color/points'
+    listener = ImageListener(topic)
+    rospy.sleep(1)
 
-    cv2.namedWindow('disp',cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('disp',800,800)
+    while not rospy.is_shutdown():
 
-    cv2.createTrackbar('numDisparities','disp',3,100,nothing)
-    cv2.createTrackbar('blockSize','disp',5,50,nothing)
-    cv2.createTrackbar('preFilterType','disp',0,1,nothing)
-    cv2.createTrackbar('preFilterSize','disp',1,25,nothing)
-    cv2.createTrackbar('preFilterCap','disp',7,62,nothing)
-    cv2.createTrackbar('textureThreshold','disp',7,100,nothing)
-    cv2.createTrackbar('uniquenessRatio','disp',12,100,nothing)
-    cv2.createTrackbar('speckleRange','disp',1,100,nothing)
-    cv2.createTrackbar('speckleWindowSize','disp',3,25,nothing)
-    cv2.createTrackbar('disp12MaxDiff','disp',2,25,nothing)
-    cv2.createTrackbar('minDisparity','disp',4,25,nothing)
-
-    left_ros_image = message_filters.Subscriber("/stereocamera/left/image_raw", Image)
-    right_ros_image =message_filters.Subscriber("/stereocamera/right/image_raw", Image)
-    ts = message_filters.TimeSynchronizer([left_ros_image , right_ros_image], 10)
-    ts.registerCallback(callback)
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+        indexX = np.unravel_index(np.argmin(np.absolute(listener.PointsX_H-FootX_H)),listener.PointsX_H.shape)
+        indexY = np.argmin(np.absolute(listener.PointsY_H[indexX[0]]-FootY_H))
+        print(indexX[0],indexY,listener.PointsZ_H[indexX[0]][indexY])
+        #print(listener.PointsZ_H[0][0])
