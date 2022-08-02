8f85291eb18dab10b5dbdfe167565498c987b365
SheldonErasmus <sheldon.erasmus@gmail.com>
2022-08-02 14:39:06 +0000

    2 aug camTest

--- catkin_ws/src/simu_hexapod_stuff/src/camTest.py
+++ catkin_ws/src/simu_hexapod_stuff/src/camTest.py
@@ -24,22 +24,21 @@ class ImageListener:
         self.sub = rospy.Subscriber(topic, numpy_msg(msg_Image), self.imageDepthCallback)
         self.sub_info = rospy.Subscriber('/camera/depth/camera_info', CameraInfo, self.imageDepthInfoCallback)
         self.intrinsics = None
-        self.cv_image = [np.ones((720,1280)) for i in range(7)]
+        self.cv_image = [np.ones((720,1280)) for i in range(1)]
         
         pose = {"x":0,"y":0, "z":0,"yaw":0}
-        self.image_pose = [pose for i in range(7)]
+        self.image_pose = [pose for i in range(1)]
 
         self.SomeTable = {}
 
     def imageDepthCallback(self, data):
         if self.flag == 1:
-            self.flag = 0 
+            self.flag = 1 
             try:
                 temp = [np.frombuffer(data.data, dtype=np.uint16).reshape(data.height, data.width)]
-                temp1 = [self.bridge.imgmsg_to_cv2(data, data.encoding)]
-                self.cv_image = np.concatenate((self.cv_image[1:7],temp))
+                self.cv_image = temp
                 pose = [{"x":n,"y":e, "z":d,"yaw":yaw}]
-                self.image_pose = np.concatenate((self.image_pose[1:7],pose))
+                self.image_pose = pose
                 self.SomeTable = {}
             except CvBridgeError as er:
                 print(er)
@@ -83,7 +82,7 @@ def imu_cb(msg):
 
 FeetPlaceXBuffer = np.ones(12)/1000
 FeetPlaceYBuffer = np.ones(12)/1000
-NewPosFlag = 0
+NewPosFlag = 1
 adjustHeightFlag = -2
 def FeetPlace_cb(msg):
     global FeetPlaceXBuffer, FeetPlaceYBuffer, NewPosFlag,adjustHeightFlag
@@ -95,7 +94,7 @@ def FeetPlace_cb(msg):
 
 def FeetOnFloor_cb(msg):
     global adjustHeightFlag
-    listener.flag = 1
+    #listener.flag = 1
     #if adjustHeightFlag == -1: adjustHeightFlag = 1
 
 
@@ -114,10 +113,8 @@ if __name__ == '__main__':
     msg = PathVar_n_cmdVel(); msg.path_var.Fh = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
 
     robot = HexapodC()
-
-    rospy.wait_for_message('WorldFeetPlace',WorldFeetPlace)
     
-    H = 130 #height of cam
+    H = 135 #height of cam
     L = 60 #X offset of cam
     CA = 30 #cam angle
     LI = 17.5 #Left imager offset
@@ -129,22 +126,22 @@ if __name__ == '__main__':
 
     while not rospy.is_shutdown():
 
-        FootXPos_world = FeetPlaceXBuffer*1000 
+        FootXPos_world = [500]#FeetPlaceXBuffer*1000 
         #FootXPos_world = np.array([450.0,450.0,450.0,600.0,600.0,600.0])
-        FootYPos_world = FeetPlaceYBuffer*1000 
+        FootYPos_world = [0]#FeetPlaceYBuffer*1000 
         #FootYPos_world = np.array([0.0,125.0,-125.0,0.0,125.0,-125.0])
 
         if NewPosFlag == 1:
-            NewPosFlag = 0
+            NewPosFlag = 1
 
             flag = [0,0,0,0,0,0]
             zmax = [0,0,0,0,0,0]
             jmin = [99999,99999,99999,99999,99999,99999]
             TransZ = np.array([0.0,0.0,0.0,0.0,0.0,0.0])
 
-            for k in range(0,6):
+            for k in range(0,1):
                 loopCounter = 0; loopBreak = 0
-                while loopCounter <= 6 and loopBreak == 0:
+                while loopCounter <= 0 and loopBreak == 0:
                     
                     n_snapshot = round(listener.image_pose[loopCounter]["x"],3)
                     e_snapshot = round(listener.image_pose[loopCounter]["y"],3)
@@ -221,48 +218,7 @@ if __name__ == '__main__':
             #print(TransZ); 
             print("\t"); print(TransZ_world)
             
-        if adjustHeightFlag == 0:
-            print(adjustHeightFlag)
-            adjustHeightFlag = -1
 
-            for k in [1,3,5]:
-                    msg.path_var.Fh[k] = msg.path_var.Fh[k+6]
-
-            for k in [0,2,4]:
-                if np.isnan(TransZ_world[k]):
-                    msg.path_var.Fh[k+6] = msg.path_var.Fh[k+6]
-                else:
-                    msg.path_var.Fh[k+6] = TransZ_world[k]
-
-            for k in [0,2,4]:
-                if msg.path_var.Fh[k+6] > msg.path_var.Fh[k]:
-                    msg.path_var.Fh[k] = msg.path_var.Fh[k+6]
-
-            msg.path_var.Sh = [50, 50, 50, 50, 50, 50]+msg.path_var.Fh[0:6]
-            #msg.path_var.Fh = TransZ_world[0:6]
-            msg.Name = 'Camera'
-            pub.publish(msg)
-        elif adjustHeightFlag == 1:
-            print(adjustHeightFlag)
-            adjustHeightFlag = -2
-
-            for k in [0,2,4]:
-                    msg.path_var.Fh[k] = msg.path_var.Fh[k+6]
-
-            for k in [1,3,5]:
-                if np.isnan(TransZ_world[k]):
-                    msg.path_var.Fh[k+6] = msg.path_var.Fh[k+6]
-                else:
-                    msg.path_var.Fh[k+6] = TransZ_world[k]
-
-            for k in [1,3,5]:
-                if msg.path_var.Fh[k+6] > msg.path_var.Fh[k]:
-                    msg.path_var.Fh[k] = msg.path_var.Fh[k+6]
-
-            msg.path_var.Sh = [50, 50, 50, 50, 50, 50]+msg.path_var.Fh[0:6]/4
-            #msg.path_var.Fh = TransZ_world[0:6]
-            msg.Name = 'Camera'
-            pub.publish(msg)
 
         
         
